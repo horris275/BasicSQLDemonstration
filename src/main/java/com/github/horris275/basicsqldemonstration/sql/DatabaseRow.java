3985cc3daa8da00c79dac17bd240734aa348a792
horris275 <lssmith99@hotmail.com>
2025-09-30 19:07:59 +0100

    WIP Initial rewrite of DatabaseRow to support dynamic columns

--- src/main/java/com/github/horris275/basicsqldemonstration/sql/DatabaseRow.java
+++ src/main/java/com/github/horris275/basicsqldemonstration/sql/DatabaseRow.java
@@ -1,33 +1,89 @@
 package com.github.horris275.basicsqldemonstration.sql;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Represents a row in the database with a unique identifier,
- * title, description, and URL.
+ * This class represents a row in a database table.
  *
- * <p>A {@code uniqueId} of {@code -1} indicates that the row
- * has not yet been assigned a persistent identifier by the database.
- * Once created, the database will update the value to reflect the identifier.</p>
+ * <p>This class is a work in progress and is not yet functioning as intended</p>
  *
  * @author horris275
- * @version 22.09.2025
+ * @version 30.09.2025
  */
 public class DatabaseRow
 {
-    private int uniqueId;
-    private final String title;
-    private final String description;
-    private final String url;
+    private final Map<String, Object> columns;
 
     /**
-     * Constructs a new {@code DatabaseRow} with the unique identifier,
-     * title, description, and URL.
-     *
-     * @param uniqueId    an integer representing the unique identifier
-     * @param title       the title of the database row
-     * @param description the description of the database row
-     * @param url         the url of the database row
+     * Constructs a new {@code DatabaseRow} without any parameters.
+     * Initialises the {@code columns} field as an empty {@link LinkedHashMap}
      */
-    public DatabaseRow(int uniqueId, String title, String description, String url)
+    public DatabaseRow()
+    {
+        this(new LinkedHashMap<>());
+    }
+
+    /**
+     * Constructs a new {@code DatabaseRow} without any parameters.
+     * Assigns the {@code columns} Map as a new {@link LinkedHashMap}
+     *
+     * @param columns
+     */
+    public DatabaseRow(Map<String, Object> columns)
+    {
+        this.columns = new LinkedHashMap<>(columns);
+    }
+
+    /**
+     * Returns the value of the given column.
+     *
+     * @param columnName the name of the column to return
+     * @return           the value of the column, or null if not present
+     */
+    public Object getColumn(String columnName)
+    {
+        return columns.get(columnName);
+    }
+
+    /**
+     * Sets a new column name and associated value in this database row.
+     *
+     * @param columnName the name of the column to insert
+     * @param value      the value associated with the column
+     */
+    public void setColumn(String columnName, Object value)
+    {
+        columns.put(columnName, value);
+    }
+
+    /**
+     * Returns an unmodifiable list of the column names.
+     *
+     * @return the unmodifiable list of column names
+     */
+    public List<String> getColumnNames()
+    {
+        return List.copyOf(columns.keySet());
+    }
+
+    /**
+     * Returns an unmodifiable {@link Map} of column names and values.
+     *
+     * @return the unmodifiable {@link Map} of column names and values
+     */
+    public Map<String, Object> getColumnValues()
+    {
+        return Collections.unmodifiableMap(columns);
+    }
+}
+
+/*
+class OldDatabaseRow
+{
+    public OldDatabaseRow(int uniqueId, String title, String description, String url)
     {
         this.uniqueId = uniqueId;
         this.title = title;
@@ -35,24 +91,11 @@ public class DatabaseRow
         this.url = url;
     }
 
-    /**
-     * Returns the unique identifier of this database row.
-     *
-     * @return the unique identifier
-     */
     public int getUniqueId()
     {
         return uniqueId;
     }
 
-    /**
-     * Sets a new unique identifier for this database row.
-     * This method only completes successfully if the identifier
-     * is -1, as used by the database insertion method to
-     * update the identifier upon creation.
-     *
-     * @param newId the new unique identifier
-     */
     public void setUniqueId(int newId)
     {
         if (this.uniqueId == -1)
@@ -61,33 +104,18 @@ public class DatabaseRow
         }
     }
 
-    /**
-     * Returns the title of this database row.
-     *
-     * @return the title
-     */
     public String getTitle()
     {
         return title;
     }
 
-    /**
-     * Returns the description of this database row.
-     *
-     * @return the description
-     */
     public String getDescription()
     {
         return description;
     }
 
-    /**
-     * Returns the URL of this database row.
-     *
-     * @return the URL
-     */
     public String getUrl()
     {
         return url;
     }
-}
+}*/
