1315e063bff141aef9aab6bd2dfa1b7654004af7
horris275 <lssmith99@hotmail.com>
2025-10-01 16:10:03 +0100

    Add uniqueId field to DatabaseRow with support for dynamic columns

--- src/main/java/com/github/horris275/basicsqldemonstration/sql/DatabaseRow.java
+++ src/main/java/com/github/horris275/basicsqldemonstration/sql/DatabaseRow.java
@@ -8,14 +8,17 @@ import java.util.Map;
 /**
  * This class represents a row in a database table.
  *
- * <p>This class is a work in progress and is not yet functioning as intended</p>
+ * <p>It contains a {@link LinkedHashMap} which represents the column
+ * name and the associated data. The unique identifier (if required) is controlled
+ * by a separate field {@code uniqueId} and must not be used within the LinkedHashMap.</p>
  *
  * @author horris275
- * @version 30.09.2025
+ * @version 01.10.2025
  */
 public class DatabaseRow
 {
     private final Map<String, Object> columns;
+    private int uniqueId;
 
     /**
      * Constructs a new {@code DatabaseRow} without any parameters.
@@ -23,18 +26,46 @@ public class DatabaseRow
      */
     public DatabaseRow()
     {
-        this(new LinkedHashMap<>());
+        this(-1, new LinkedHashMap<>());
     }
 
     /**
      * Constructs a new {@code DatabaseRow} with a predefined column data.
      * Assigns the {@code columns} Map as a new {@link LinkedHashMap}
      *
-     * @param columns the initial column names and values to populate this row
+     * @param uniqueId an integer representing the unique identifier
+     * @param columns  the initial column names and values to populate this row
      */
-    public DatabaseRow(Map<String, Object> columns)
+    public DatabaseRow(int uniqueId, Map<String, Object> columns)
     {
         this.columns = new LinkedHashMap<>(columns);
+        this.uniqueId = uniqueId;
+    }
+
+    /**
+     * Returns the unique identifier of this database row.
+     *
+     * @return the unique identifier
+     */
+    public int getUniqueId()
+    {
+        return uniqueId;
+    }
+
+    /**
+     * Sets a new unique identifier for this database row.
+     * This method only completes successfully if the identifier
+     * is -1, as used by the database insertion method to
+     * update the identifier upon creation.
+     *
+     * @param newId the new unique identifier
+     */
+    public void setUniqueId(int newId)
+    {
+        if (uniqueId == -1)
+        {
+            uniqueId = newId;
+        }
     }
 
     /**
